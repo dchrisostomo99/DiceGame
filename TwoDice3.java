0885b50384d205fcd8d39eba7496eb5c4603cb64
dchrisostomo99 <38891961+dchrisostomo99@users.noreply.github.com>
2018-08-07 14:13:56 -0400

    Update TwoDice3.java

--- TwoDice3.java
+++ TwoDice3.java
@@ -1,3 +1,5 @@
+//This is a dice game that will compare the outcome of twi dice and determine a winner
+//This will import the Scanner class only to use later when to collect info from user
 import java.util.Scanner;
 
 public class TwoDice3
@@ -5,7 +7,11 @@ public class TwoDice3
     public static void main(String[] args) 
     {
         int response;
+        //Constructs a Scanner object to take in user input
         Scanner kbd = new Scanner(System.in);
+        // a do loop that will show and compare the outcome of the die and test 
+        //to see which one is bigger or if they are the same and declare which die is bigger
+        //Then asks user if they want to play again
         do
         {
             Die Die1 = new Die();    
@@ -39,7 +45,8 @@ public class TwoDice3
             System.out.print("Type 1 to play again type any other to quit");
             response = kbd.nextInt();
         }
-
+        //while loop that continues to run as long as response equals one
+        //If not exit message will appear 
         while(response == 1);
         System.out.println("You will be missed");
     }
